7fb3f3587bc54261648bbb687d25ec8988cbd4fe
navyaranjan <mails2navya@gmail.com>
2019-07-23 21:34:51 +1000

    Switched to Gradle

--- src/main/java/domainregistrar/App.java
+++ src/main/java/domainregistrar/App.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.*;
 public class App {
 
     private List<DomainType> domainTypes = new ArrayList<>();
